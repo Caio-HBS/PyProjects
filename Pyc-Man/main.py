91afacc06e66d2926e19a694a90e779544d332df
Caio Bianchi <caiohbs23@gmail.com>
2023-05-25 10:36:17 -0300

    Changes on sprites and on initial position

--- Pyc-Man/main.py
+++ Pyc-Man/main.py
@@ -3,8 +3,8 @@ import os
 
 
 # Handles the window size and name.
-WIDTH, HEIGHT = 700, 700
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 512, 512
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("PYC-MAN")
 
 
@@ -12,37 +12,75 @@ pygame.display.set_caption("PYC-MAN")
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+# Base width and height for sprites.
+SPRITE_WIDTH, SPRITE_HEIGHT = 22, 22
 
 # Handles the sprites.
-PAC_MAN = pygame.image.load(os.path.join("Assets", "pac-man.png"))
-VULNERABLE_GHOST = pygame.image.load(os.path.join("Assets", "vulnerable_ghost.png"))
-RED_GHOST = pygame.image.load(os.path.join("Assets", "red_ghost.png"))
-PINK_GHOST = pygame.image.load(os.path.join("Assets", "pink_ghost.png"))
-CYAN_GHOST = pygame.image.load(os.path.join("Assets", "cyan_ghost.png"))
-ORANGE_GHOST = pygame.image.load(os.path.join("Assets", "orange_ghost.png"))
-SMALL_DOTS = pygame.image.load(os.path.join("Assets", "small_dots.png"))
-BIG_DOTS = pygame.image.load(os.path.join("Assets", "big_dots.png"))
+PAC_MAN_IMAGE = pygame.image.load(
+    os.path.join("Assets", "pac-man.png"))
+PAC_MAN = pygame.transform.scale(PAC_MAN_IMAGE, (SPRITE_WIDTH, SPRITE_HEIGHT))
+
+# VULNERABLE_GHOST = pygame.image.load(
+#     os.path.join("Assets", "vulnerable_ghost.png"))
+
+# RED_GHOST = pygame.image.load(
+#     os.path.join("Assets", "red_ghost.png"))
+
+# PINK_GHOST = pygame.image.load(
+#     os.path.join("Assets", "pink_ghost.png"))
+
+# CYAN_GHOST = pygame.image.load(
+#     os.path.join("Assets", "cyan_ghost.png"))
+
+# ORANGE_GHOST = pygame.image.load(
+#     os.path.join("Assets", "orange_ghost.png"))
+
+# SMALL_DOTS = pygame.image.load(
+#     os.path.join("Assets", "small_dots.png"))
+
+# BIG_DOTS = pygame.image.load(
+#     os.path.join("Assets", "big_dots.png"))
 
 
 # FPS cap variable.
 FPS = 60
+VELOCITY = 1.1
 
 
-def draw_window():
-    WIN.fill(BLACK)
+def pac_man_movement(keys, pac_hitbox):
+    if keys[pygame.K_UP] and (pac_hitbox.y) > 26:
+        pac_hitbox.y -= VELOCITY
+    elif keys[pygame.K_DOWN] and (pac_hitbox.y) < 462:
+        pac_hitbox.y += VELOCITY
+    elif keys[pygame.K_LEFT] and (pac_hitbox.x) > 26:
+        pac_hitbox.x -= VELOCITY
+    elif keys[pygame.K_RIGHT] and (pac_hitbox.x) < 460:
+        pac_hitbox.x += VELOCITY
+
+
+def draw_window(pac_hitbox):
+    WIN.blit(PAC_MAN, (pac_hitbox.x, pac_hitbox.y))
     pygame.display.update()
 
 
 def main():
+    pac_hitbox = pygame.Rect(((WIDTH//2) - 10), ((HEIGHT//2) + 20), SPRITE_WIDTH, SPRITE_HEIGHT)
     clock = pygame.time.Clock()
     run = True
+    background_image = pygame.image.load(os.path.join("Assets", "background.png"))
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            elif event.type == pygame.VIDEORESIZE:
+                pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+        WIN.blit(background_image, (6, 6))
+        
+        keys = pygame.key.get_pressed()
+        pac_man_movement(keys, pac_hitbox)
 
-        draw_window()
+        draw_window(pac_hitbox)
 
     pygame.quit()
 
