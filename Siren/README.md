4df9806084e9abfd959b53933f57dc1495014cf6
Caio Bianchi <caiohbs23@gmail.com>
2023-08-31 13:31:16 -0300

    added Siren project

---
+++ Siren/README.md
@@ -0,0 +1,2 @@
+The following project aims to create a Python application that takes in a folder
+of images and compare them to point out the duplicates.
---
+++ Siren/main.py
@@ -0,0 +1,28 @@
+import os
+import json
+import shutil
+
+from difPy import dif
+
+search = dif(input("Select directory to analyze"))
+new_directory = input("Select new directory for duplicates")
+
+
+def change_duplicates_directory(
+        list_of_paths_of_duplicates: list, 
+        new_directory_for_duplicates
+):
+    """
+    Takes the list of file paths provided by difPy and changes those files to a 
+    new folder.
+        Args:
+            list_of_paths_of_duplicates: list of duplicates provided by difPy;
+            new_directory_for_duplicates: path for the new directory where 
+            duplicates will be stored.
+    """
+    for single_file_path in list_of_paths_of_duplicates:
+        file_name = os.path.basename(single_file_path)
+        new_directory_path = os.path.join(new_directory_for_duplicates, file_name)
+        shutil.move(single_file_path, new_directory_path)
+
+change_duplicates_directory(search.lower_quality, new_directory)
