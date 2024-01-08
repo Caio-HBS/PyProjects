550b06c28b3dca87c28d04251c7ef24e294844a1
Caio Bianchi <caiohbs23@gmail.com>
2024-01-08 11:44:58 -0300

    progress on game.py and updated README

--- Tic-tac_toe/README.md
+++ Tic-tac_toe/README.md
@@ -1,1 +1,29 @@
+# tic_tac_toe.py
+
 A Python implementation of the classic Tic-tac toe game.
+
+## Usage
+
+```sh
+# cd into the folder (if you're not into it already)
+cd Tic-tac_toe
+
+# create a virtual environment for python and activate it (only valid for Linux and MacOS)
+python3 -m venv .venv
+source .venv/bin/activate
+
+# run the main python script
+python3 main.py
+```
+
+### For Windows users
+
+If you're using Windows, you'll need to use different commands, but the script to be used is the same `main.py` located in the main project folder.
+
+Please use this guide to create and activate a virtual environment in Windows:
+
+***[How to create/activate a venv in Windows](https://docs.python.org/3/library/venv.html)*** (official Python documentation)
+
+## Final Thoughts
+
+TODO
--- Tic-tac_toe/game.py
+++ Tic-tac_toe/game.py
@@ -5,9 +5,11 @@ import ttkbootstrap as ttk
 # TODO: Add functions.
 # TODO: Add final screen to show success or failure.
 
+
 class MainWindow:
     def __init__(self, root):
         self.root = root
+        self.game_grid = [[None, None, None], [None, None, None], [None, None, None]]
         self.root.title("Tic-Tac Toe")
         style = ttk.Style(theme="solar")
         style.theme_use()
@@ -33,6 +35,7 @@ class MainWindow:
             padding=10,
             cursor="hand2",
             style="primary.TButton",
+            command=lambda: self.kill_current_screen_and_draw_new_one(self.game_screen),
         )
         start_game_button.pack(pady=165)
 
@@ -41,3 +44,54 @@ class MainWindow:
 
         version = ttk.Label(master=self.root, text="ver. 0.0.1")
         version.pack(anchor="se")
+
+    def game_screen(self):
+        """
+        Renders the game screen.
+        """
+        player_title = ttk.Label()
+        player_title.pack()
+
+        tittle_main_menu = ttk.Label(
+            master=self.root, text="Tic-Tac Toe", font="Arial 42 bold", padding=10
+        )
+
+        tittle_main_menu.pack()
+
+    def kill_current_screen_and_draw_new_one(self, new_window):
+        """
+        Kills the current drawn screen and calls the new one.
+
+        Args:
+            new_window: the window to be rendered after killing the last one.
+        """
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        new_window()
+
+    def check_game_status(self) -> bool:
+        """
+        Checks if the game is over and returns a boolean based on it.     
+        """
+
+        # Three consecutive "X's" or "O's" in the same a row.
+        for row in self.game_grid:
+            if all(cell == row[0] for cell in row if cell is not None):
+                return True 
+
+        # Three consecutive "X's" or "O's" in the same a column.
+        for col in range(3):
+            if all(row[col] == self.game_grid[0][col] for row in self.game_grid if row[col] is not None):
+                return True
+
+        # Three consecutive "X's" or "O's" in the main diagonal (NW to SE).
+        if all(self.game_grid[i][i] == self.game_grid[0][0] for i in range(3) if self.game_grid[i][i] is not None):
+            return True
+
+        # Three consecutive "X's" or "O's" in the secondary diagonal (SW to NE).
+        if all(self.game_grid[i][2 - i] == self.game_grid[0][2] for i in range(3) if self.game_grid[i][2 - i] is not None):
+            return True
+
+        # No winners.
+        return False
