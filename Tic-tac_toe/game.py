3cae093b01f0725c8ab1d95ec1b74f2e14ec71db
Caio Bianchi <caiohbs23@gmail.com>
2024-01-08 12:02:19 -0300

    Added game_grid

--- Tic-tac_toe/game.py
+++ Tic-tac_toe/game.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 
-# TODO: Add game screen.
+# TODO: Add player tracking.
 # TODO: Add functions.
 # TODO: Add final screen to show success or failure.
 
@@ -49,14 +49,41 @@ class MainWindow:
         """
         Renders the game screen.
         """
-        player_title = ttk.Label()
-        player_title.pack()
-
-        tittle_main_menu = ttk.Label(
-            master=self.root, text="Tic-Tac Toe", font="Arial 42 bold", padding=10
+        player1_label = ttk.Label(
+            master=self.root, text="Player 1", font="Arial 24", padding=10
         )
+        player1_label.grid(row=0, column=0, padx=10, pady=10)
 
-        tittle_main_menu.pack()
+        player2_label = ttk.Label(
+            master=self.root, text="Player 2", font="Arial 24 bold", padding=10
+        )
+        player2_label.grid(row=0, column=1, padx=10, pady=10)
+
+        # Creating a 3x3 grid of buttons
+        for i in range(3):
+            for j in range(3):
+                button = ttk.Button(
+                    master=self.root,
+                    text="",
+                    command=lambda i=i, j=j: self.on_button_click(i, j),
+                )
+                button.grid(row=i + 1, column=j, padx=10, pady=10)
+
+    def on_button_click(self, row: int, col: int, current_player: str):
+        """
+        Handles updating the `game_grid` as well as the styling on the player labels.
+
+        Args:
+            row: the row of the pressed button;
+            col: the column of the pressed button;
+            current_player: the player who clicked the button.
+        """
+        # Prevents from clicking cells with values:
+        if self.game_grid[row][col] is None:
+            self.game_grid[row][col] = "X" # TODO: add styling change on the labels based on who's turn it is.
+            print(self.game_grid)
+
+        
 
     def kill_current_screen_and_draw_new_one(self, new_window):
         """
@@ -72,25 +99,37 @@ class MainWindow:
 
     def check_game_status(self) -> bool:
         """
-        Checks if the game is over and returns a boolean based on it.     
+        Checks if the game is over and returns a boolean based on it.
         """
 
         # Three consecutive "X's" or "O's" in the same a row.
         for row in self.game_grid:
             if all(cell == row[0] for cell in row if cell is not None):
-                return True 
+                return True
 
         # Three consecutive "X's" or "O's" in the same a column.
         for col in range(3):
-            if all(row[col] == self.game_grid[0][col] for row in self.game_grid if row[col] is not None):
+            if all(
+                row[col] == self.game_grid[0][col]
+                for row in self.game_grid
+                if row[col] is not None
+            ):
                 return True
 
         # Three consecutive "X's" or "O's" in the main diagonal (NW to SE).
-        if all(self.game_grid[i][i] == self.game_grid[0][0] for i in range(3) if self.game_grid[i][i] is not None):
+        if all(
+            self.game_grid[i][i] == self.game_grid[0][0]
+            for i in range(3)
+            if self.game_grid[i][i] is not None
+        ):
             return True
 
         # Three consecutive "X's" or "O's" in the secondary diagonal (SW to NE).
-        if all(self.game_grid[i][2 - i] == self.game_grid[0][2] for i in range(3) if self.game_grid[i][2 - i] is not None):
+        if all(
+            self.game_grid[i][2 - i] == self.game_grid[0][2]
+            for i in range(3)
+            if self.game_grid[i][2 - i] is not None
+        ):
             return True
 
         # No winners.
